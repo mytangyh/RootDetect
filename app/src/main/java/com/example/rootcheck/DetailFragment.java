f02c10d38524189a772ddf9c358dae2b7e3d5185
mytangyh <mytangyh@163.com>
2023-11-07 10:32:32 +0800

    viewpager

--- app/src/main/java/com/example/rootcheck/DetailFragment.java
+++ app/src/main/java/com/example/rootcheck/DetailFragment.java
@@ -1,10 +1,14 @@
 package com.example.rootcheck;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +25,7 @@ public class DetailFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private WebView webView;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -56,7 +61,42 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        webView=view.findViewById(R.id.webview);
+        return view;
+    }
+    private void show(){
+        String str ="";
+            str = "<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
+                    "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=gbk\">\n" +
+                    "  <title>GBK Encoded HTML</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "  <h1>Hello, 你好</h1>\n" +
+                    "  <p>This is an example of GBK encoded HTML content.</p>\n" +
+                    "</body>\n" +
+                    "</html>\n";
+        webView.loadData(str,"text/html; charset=gbk",null);
+
+        }
+
+    /**
+     * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
+     * has returned, but before any saved state has been restored in to the view.
+     * This gives subclasses a chance to initialize themselves once
+     * they know their view hierarchy has been completely created.  The fragment's
+     * view hierarchy is not however attached to its parent at this point.
+     *
+     * @param view               The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     */
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        show();
     }
 }
