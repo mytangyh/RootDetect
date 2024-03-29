2f97282414ba3997f10ff8dab4d56a88200dab8a
mytangyh <mytangyh@163.com>
2024-03-30 01:40:17 +0800

    检测代理 代码优化

--- library/src/main/java/com/example/lib/ProxyDetector.kt
+++ library/src/main/java/com/example/lib/ProxyDetector.kt
@@ -3,59 +3,53 @@ package com.example.lib
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Build
 import java.net.NetworkInterface
 import java.net.SocketException
 
-
+/**
+ * 这个类用于检测设备是否正在使用代理。
+ */
 class ProxyDetector {
     companion object {
+        /**
+         * 检查设备是否检测到代理。
+         *
+         * @param context 应用程序上下文。
+         * @return 如果检测到代理则返回true，否则返回false。
+         */
         fun isDetected(context: Context): Boolean {
-            return checkHttpProxy()||isVpnConnected(context)||isDeviceInVPN()
+            return checkHttpProxy() || isVpnConnected(context) || isDeviceInVPN()
         }
 
-        fun checkHttpProxy(): Boolean {
+        /**
+         * 检查设备是否设置了HTTP代理。
+         *
+         * @return 如果设置了HTTP代理则返回true，否则返回false。
+         */
+        private fun checkHttpProxy() = System.getProperty("http.proxyHost")?.isNotEmpty() == true && System.getProperty("http.proxyPort")?.isNotEmpty() == true
 
-            val proxyHost: String? = System.getProperty("http.proxyHost")
-            val proxyPort: String? = System.getProperty("http.proxyPort")
-
-            return !proxyHost.isNullOrEmpty() && !proxyPort.isNullOrEmpty()
-
-        }
-
-        fun isVpnConnected(context: Context): Boolean {
+        /**
+         * 检查设备是否连接了VPN。
+         *
+         * @param context 应用程序上下文。
+         * @return 如果连接了VPN则返回true，否则返回false。
+         */
+        private fun isVpnConnected(context: Context): Boolean {
             val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                val network = cm.activeNetwork
-                if (network != null) {
-                    val capabilities = cm.getNetworkCapabilities(network)
-                    if (capabilities != null) {
-                        return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
-                    }
-                }
-            } else {
-                val networkInfo = cm.activeNetworkInfo
-                if (networkInfo != null) {
-                    return networkInfo.type == ConnectivityManager.TYPE_VPN
-                }
-            }
-            return false
+            val network = cm.activeNetwork ?: return false
+            val capabilities = cm.getNetworkCapabilities(network) ?: return false
+            return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
         }
 
-        fun isDeviceInVPN(): Boolean {
-            try {
-                val networkInterfaces = NetworkInterface.getNetworkInterfaces()
-                while (networkInterfaces.hasMoreElements()) {
-                    val name = networkInterfaces.nextElement().name
-                    if (name == "tun0" || name == "ppp0") {
-                        return true
-                    }
-                }
-            } catch (e: SocketException) {
-                e.printStackTrace()
-            }
-            return false
+        /**
+         * 检查设备是否在VPN中。
+         *
+         * @return 如果设备在VPN中则返回true，否则返回false。
+         */
+        private fun isDeviceInVPN() = try {
+            NetworkInterface.getNetworkInterfaces().asSequence().any { it.name in listOf("tun0", "ppp0") }
+        } catch (e: SocketException) {
+            false
         }
     }
-
 }
